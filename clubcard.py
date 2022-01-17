47385320b30b9224e671bb794c8d79e7da200c69
Haga István Dávid <hagaistvandavid@EOTVOS2.LOCAL>
2022-01-17 13:16:38 +0100

    kártya generátor és gyartasgenerator plussz pontszam

---
+++ clubcard.py
@@ -0,0 +1,23 @@
+def cardGenerator():
+    szamlista=["123456789"]
+    betulista=["qwertzuioplkjhgfdsayxcvbnm"]
+    return cardGenerator
+
+def gyartasGenerator():
+    evlista=["1988,2001,2006,2010,2021,2022"]
+    return gyartasGenerator
+
+pontszam=100
+eredmeny=("")
+ev=2005
+if ev==1988 or ev==2001:
+    100+150
+    print(eredmeny)
+if 2006 or 2021 +100*2:
+    print(eredmeny)
+if 2022 and +200%80:
+    print(eredmeny)
+else: 2022 -3
+    
+
+
